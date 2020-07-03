e93db076962def658249a41bcaae3c5466bea472
Thiago Gouvêa <thiagonunes026@gmail.com>
2020-07-03 14:24:27 -0300

    Front-End do Projeto

---
+++ src/layouts/Dashboard/components/NavBar/navigationConfig.js
@@ -0,0 +1,55 @@
+/* eslint-disable react/no-multi-comp */
+/* eslint-disable react/display-name */
+import React from 'react';
+import { colors } from '@material-ui/core';
+import EventNote from '@material-ui/icons/EventNote';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import ChatIcon from '@material-ui/icons/ChatOutlined';
+import CodeIcon from '@material-ui/icons/Code';
+import DashboardIcon from '@material-ui/icons/DashboardOutlined';
+import HomeIcon from '@material-ui/icons/HomeOutlined';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PersonIcon from '@material-ui/icons/PersonOutlined';
+import PlaceIcon from '@material-ui/icons/PlaceOutlined';
+import AssignmentIndOutlinedIcon from '@material-ui/icons/AssignmentIndOutlined';
+
+import { Label } from 'components';
+
+export default [
+  {
+    title: 'Menu',
+    pages: [
+      {
+        title: 'Lojas',
+        href: '/store',
+        icon: AssignmentIndOutlinedIcon
+      },
+      {
+        title: 'Cupons',
+        href: '/coupon',
+        icon: PlaceIcon
+      },
+      {
+        title: 'Pessoas',
+        href: '/person',
+        icon: ListAltIcon
+      },
+      {
+        title: 'Documentos',
+        href: '/document',
+        icon: ListAltIcon
+      },
+      {
+        title: 'Servidor',
+        href: '/server',
+        icon: ListAltIcon
+      },
+      {
+        title: 'File',
+        href: '/file',
+        icon: ListAltIcon
+      }
+    ]
+  },
+  
+];
